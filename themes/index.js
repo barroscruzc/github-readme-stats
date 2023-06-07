bd370f7d33feadeb689e5b3db92105788d21b221
Cristhiane da Cruz <93226440+barroscruzc@users.noreply.github.com>
2023-06-07 13:58:00 -0300

    Update index.js

--- themes/index.js
+++ themes/index.js
@@ -423,6 +423,13 @@ export const themes = {
     border_color: "85A4C0",
     bg_color: "030314",
   },
+  ambient_gradient: {
+    title_color: "#FFFFFF",
+    text_color: "#FFFFFF",
+    icon_color: "#FFFFFF",
+    border_color:"#AE58A1",
+    bg_color: "35,4158D0,C850C0,FFCC70",
+  },
 };
 
 export default themes;
